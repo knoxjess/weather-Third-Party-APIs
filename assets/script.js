b0032a83f35d4806ff46610834d0f81ac100b0e4
knoxjess <110634800+knoxjess@users.noreply.github.com>
2023-02-02 19:53:25 -0600

    api added

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+// searches the city 
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+// API key
+var APIKey="cf6252a1b37109cb680219e894b9393d";
+// Display the curent and future weather 
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
